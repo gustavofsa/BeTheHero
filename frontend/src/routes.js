4f6b239039dec20c7a8604eef7c826e8dae7ca3a
gustavofsa <gugafarias2009@gmail.com>
2020-03-29 19:47:42 -0300

    criação das páginas profile e new incidents

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/logon';
 import Register from './pages/register';
+import Profile from './pages/profile';
+import NewIncident from './pages/newIncident';
 
 export default function Routes() {
   return (
@@ -10,6 +12,9 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
