f9e83d6b32c1bc1d1f75e1f8a05cdfe0af45940c
gustavofsa <gugafarias2009@gmail.com>
2020-03-29 22:33:24 -0300

    finalização do frontend e conexão com backend

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +9,37 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login, tente novamente.');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Heroe" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID"/>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
 
           <Link className="back-link" to="/register">
--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId,
+      }
+    }).then(response =>{
+      setIncidents(response.data);
+    })
+  }, [ongId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      setIncidents(incidents.filter(incidents => incidents.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.')
+    }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+  <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         
-          <button type="button">
+          <button onClick={handleLogout} type="button">
             <FiPower size={18} color="#e02041" />
           </button>
       </header>
@@ -23,64 +62,22 @@ export default function Profile() {
       <h1>Casos cadastrados</h1>
 
       <ul>
-        <li>
+       {incidents.map(incident =>(
+          <li key={incident.id}>
           <strong>CASO:</strong>
-          <p>Caso teste</p>
+          <p>{incident.title}</p>
 
           <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
+          <p>{incident.description}</p>
 
           <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
+          <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-          <button type="button">
+          <button onClick={() => handleDeleteIncident(incident.id)} type="button">
             <FiTrash2 size={20} color="#a8a8b3" />
           </button>
         </li>
-
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+       ))}
       </ul>
     </div>
   );
