303d6c842cd1109597457b2dacff3bba24775015
gustavofsa <gugafarias2009@gmail.com>
2020-04-04 01:10:30 -0300

    Mobile da aplicação

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -37,4 +37,40 @@ export default StyleSheet.create ({
         color: '#737380'
     },
 
+
+    incidentList: {
+        marginTop: 32,
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    detailsButtonText: {
+        color: '#e02041',
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
 });
